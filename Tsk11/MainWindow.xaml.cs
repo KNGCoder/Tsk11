99c87317d445ecc6e2ef433f0e5df3a927f54ac1
Figaro Pomni <kngsklad@gmail.com>
2023-12-21 21:35:53 +0300

    Добавьте файлы проекта.

---
+++ Tsk11/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tsk11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ToUpperValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.ToString().ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
